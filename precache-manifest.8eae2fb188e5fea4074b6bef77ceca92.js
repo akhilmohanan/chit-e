bbb2b1f6d4179876dabcff578df9e27d616491c7
Akhil M <akhilmohanan@qburst.com>
2020-04-04 23:16:45 +0530

    Updates

---
+++ precache-manifest.8eae2fb188e5fea4074b6bef77ceca92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541645b520d3908a74224753acd9f945",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "550a43fa5ecbcb846d73",
+    "url": "http://localhost:3000/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47c118168e1434d5bbec",
+    "url": "http://localhost:3000/static/js/2.c3a03266.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://localhost:3000/static/js/2.c3a03266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550a43fa5ecbcb846d73",
+    "url": "http://localhost:3000/static/js/main.7d5da8c8.chunk.js"
+  },
+  {
+    "revision": "16a3593fd047c891a8fd",
+    "url": "http://localhost:3000/static/js/runtime-main.649c58fa.js"
+  }
+]);
